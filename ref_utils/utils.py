983e3fa1346fcbe50cd86e9cf37baf130dc6c960
mschloegel <moritz.schloegel@rub.de>
2020-05-10 12:25:13 +0200

    fix type annotations; fix colored prints overwriting sep; add run_shell (discuss?)

--- ref_utils/utils.py
+++ ref_utils/utils.py
@@ -1,5 +1,5 @@
-"""Functions related to colored printing"""
-from typing import List, Optional
+""" Utility functions including colored printing or subprocess.run wrapper dropping privileges"""
+from typing import Any, List, Optional
 import subprocess
 
 from colorama import Fore, Style
@@ -9,26 +9,45 @@ from .process import drop_privileges
 SUCCESS = True
 FAILURE = False
 
-def print_ok(*args, **kwargs):
+def print_ok(*args: str, **kwargs: Any) -> None:
     """Print green to signal correctness"""
-    print(Fore.GREEN, *args, Style.RESET_ALL, **kwargs, sep='')
+    _sep = kwargs.get('sep', ' ')
+    print(Fore.GREEN + _sep.join(args) + Style.RESET_ALL, kwargs)
 
-def print_warn(*args, **kwargs):
+def print_warn(*args: str, **kwargs: Any) -> None:
     """Print yellow to warn user"""
-    print(Fore.YELLOW, *args, Style.RESET_ALL, **kwargs, sep='')
+    _sep = kwargs.get('sep', ' ')
+    print(Fore.YELLOW + _sep.join(args) + Style.RESET_ALL, kwargs)
 
-def print_err(*args, **kwargs):
+def print_err(*args: str, **kwargs: Any) -> None:
     """Print red to alert user"""
-    print(Fore.RED, *args, Style.RESET_ALL, **kwargs, sep='')
+    _sep = kwargs.get('sep', ' ')
+    print(Fore.RED + _sep.join(*args) + Style.RESET_ALL, kwargs)
 
 @drop_privileges
-def run(cmd: List[str], check_returncode: bool = False, timeout: int = 60) -> Optional[str]:
+def run(cmd: List[str], check_returncode: bool = False, timeout: int = 10) -> Optional[str]:
     """
     Execute a command (with default timeout of 60s)
     """
     output: Optional[str] = None
     try:
         p = subprocess.run(cmd, check=check_returncode, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                           timeout=timeout)
+        output = p.stdout.decode().strip()
+    except subprocess.TimeoutExpired:
+        print_err(f"[!] Unexpected timeout for: {' '.join(cmd)} (after {timeout}s)")
+    except subprocess.CalledProcessError as err:
+        print_err(f"[!] Unexpected error: {err}")
+    return output
+
+@drop_privileges
+def run_shell(cmd: List[str], check_returncode: bool = False, timeout: int = 10) -> Optional[str]:
+    """
+    Execute a command (with default timeout of 60s)
+    """
+    output: Optional[str] = None
+    try:
+        p = subprocess.run(' '.join(cmd), shell=True, check=check_returncode, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                            timeout=timeout)
         output = p.stdout.decode().strip()
     except subprocess.TimeoutExpired:
