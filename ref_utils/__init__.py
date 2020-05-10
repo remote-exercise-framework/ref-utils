996895b459bb288aad22a20c4a54dc2a113d1d94
mschloegel <moritz.schloegel@rub.de>
2020-05-10 12:26:16 +0200

    update

--- ref_utils/__init__.py
+++ ref_utils/__init__.py
@@ -1,6 +1,6 @@
 """Import functions to avoid .dot import for user""" # pylint: disable = invalid-name
-__all__ = ['process', 'assertion', 'utils']
+__all__ = ['process', 'assertion', 'checks', 'utils']
 from .process import drop_privileges, drop_privileges_to
 from .assertion import assert_is_dir, assert_is_exec, assert_is_file
-from .utils import print_ok, print_warn, print_err
+from .utils import print_ok, print_warn, print_err, run, run_shell
 from .checks import run_mypy, run_pylint, contains_flag
